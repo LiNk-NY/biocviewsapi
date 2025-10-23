894b61c5f298707ddb0a3df4a69ee7d29ab78ce6
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-10-23 17:16:19 -0400

    add instructions for daemon deployment

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,9 @@
 docker build -t my-plumber-api .
 
 docker run -it -p 8080:8000 my-plumber-api:latest /app/plumber2_ndjson.R
+
+# sudo cp biocviewsapi.service /etc/systemd/system/biocviewsapi.service
+#
+# sudo systemctl daemon-reload
+# sudo systemctl start biocviewsapi.service
+# sudo systemctl enable biocviewsapi
